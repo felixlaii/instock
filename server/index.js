2e5b4eacc444527333ac23a4b41a182f6e598a8e
Eddie <ed201112@gmail.com>
2021-11-15 15:33:54 -0800

    added basic routes

--- server/index.js
+++ server/index.js
@@ -3,9 +3,15 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 const cors = require('cors')
+const warehouses = require("./routes/Warehouses");
+const inventory = require("./routes/Inventory");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use('/warehouses', warehouses);
+app.use('/inventory', inventory);
+
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
