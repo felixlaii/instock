75788d3fc66e73ff181ea27ddb2f1c5db8a0c9b9
kailen shanahan <kailenshanahan@yahoo.ca>
2021-11-16 20:38:13 -0500

    fixed unused import

--- server/routes/Inventory.js
+++ server/routes/Inventory.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
 router.route('/item/:id')
     .get((req, res) => {
         let itemsList = require('../data/inventories.json');
