a08b2c19ea4df1367f4b4acaff5a8d765480ad6d
kailen shanahan <kailenshanahan@yahoo.ca>
2021-11-17 19:09:21 -0500

    created api functionality for creating a new item, with validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,28 @@
 import { Component } from 'react';
-import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import PageHeader from '../src/components/PageHeader/PageHeader';
 
 class App extends Component {
-  
-  render () {
+
+  render() {
     return (
       <>
-      <PageHeader />
-      <BrowserRouter>
-        <div className="App">
-          <Switch>
-            <Route path="/"/>
-            <Route path="/warehouse-details"/>
-            <Route path="/edit-warehouse"/>
-            <Route path="/add-new-warehouse"/>
-            <Route path="/inventory"/>
-            <Route path="/inventory-item-details"/>
-            <Route path="/edit-inventory-item-"/>
-            <Route path="/add-new-inventory-item-"/>
-          </Switch>
-
-        </div>
-      </BrowserRouter>  
-      </>    
+        <PageHeader />
+        <BrowserRouter>
+          <div className="App">
+            <Switch>
+              <Route path="/" />
+              <Route path="/warehouse-details" />
+              <Route path="/edit-warehouse" />
+              <Route path="/add-new-warehouse" />
+              <Route path="/inventory" />
+              <Route path="/inventory-item-details" />
+              <Route path="/edit-inventory-item-" />
+              <Route path="/add-new-inventory-item-" />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </>
     );
   }
 }
