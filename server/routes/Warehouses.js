2e1ccf2b8a1c3e0454d7f3079d6c1d17113c5a37
Eddie <ed201112@gmail.com>
2021-11-21 00:24:24 -0800

    delete warehouse back end

--- server/routes/Warehouses.js
+++ server/routes/Warehouses.js
@@ -3,16 +3,33 @@ const router = express.Router();
 const fs = require('fs')
 const path = require("path");
 
-
-const warehouses = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../data/warehouses.json")));
-
-router.get("/", (req, res) => {
-    if (warehouses){
+router.get("/", (req, res, next) => {
+    try {
+        const warehouses = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../data/warehouses.json")));
         res.status(200);
         res.json(warehouses);
-    } else {
-        res.status(404).send("warehouses not found");
-    }   
+    } catch (error) {
+        res.status(404);
+        next(error);
+    }  
+});
+
+router.delete("/delete-warehouse/:warehouseId", (req, res, next) => {
+    try {
+        const warehouses = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../data/warehouses.json")));
+        const inventory = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../data/inventories.json")));
+        const newWarehouses = warehouses.filter(item=>item.id!==req.params.warehouseId);
+        const newIventories = inventory.filter(item=>item.warehouseID!==req.params.warehouseId);
+        if (warehouses.length === newWarehouses.length || inventory.length === newIventories.length) {
+            throw new Error(`Warehouse with id=${req.params.warehouseId} not found`);
+        }
+        fs.writeFile(path.resolve(__dirname, "../data/warehouses.json"), JSON.stringify(newWarehouses));
+        fs.writeFile(path.resolve(__dirname, "../data/inventories.json"), JSON.stringify(newIventories));
+        
+    } catch (error) {
+        res.status(404);
+        next(error);
+    }
 });
 
 module.exports = router;
