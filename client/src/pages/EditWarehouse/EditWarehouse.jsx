144e5644263d45dc7ae4401574cf9e6415c8ff74
felixlaii <felix.lai@hotmail.com>
2021-11-21 11:51:08 -0500

    form validation and put method complete

--- client/src/pages/EditWarehouse/EditWarehouse.jsx
+++ client/src/pages/EditWarehouse/EditWarehouse.jsx
@@ -7,7 +7,14 @@ class EditWarehouse extends Component {
 
     state = {
         selectedWarehouse: {},
-        warehouseList: []
+        warehouseList: [],
+        // formData: {
+        //     name: '',
+        //     address: '',
+        //     city: '',
+        //     country: ''
+        // }
+
     }
 
     getSelectedWarehouse = (warehouseId) => {
@@ -34,27 +41,49 @@ class EditWarehouse extends Component {
         .catch((error) => (error))
     }
 
-    // componentDidUpdate(previousProps) {
-    //     const previousWarehouseId = previousProps.match.params.warehouseId
-    //     const currentWarehouseId = this.props.match.params.warehouseId
+    componentDidUpdate(previousProps) {
+        const previousWarehouseId = previousProps.match.params.warehouseId
+        const currentWarehouseId = this.props.match.params.warehouseId
 
-    //     if(previousWarehouseId !== currentWarehouseId) {
-    //         this.getSelectedWarehouse(currentWarehouseId)
-    //     }
-    // }
+        if(previousWarehouseId !== currentWarehouseId) {
+            this.getSelectedWarehouse(currentWarehouseId)
+        }
+    }
 
     editSuccess = (e) => {
         e.preventDefault();
-        // const data = new formData(event.target);
-        // const value = data.get('name')
-        // axios.get(`http://localhost:8080/Warehouses`, {
-        //     "name": e.target.name.value,
-        //     "address": e.target.address.value,
-        //     "city": e.target.city.value,
-        //     "country": e.target.country.value,
-        // })
-        // console.log()
-    }
+        console.log('edited')
+        const formData = e.target;
+        const name = formData.name.value
+        const address = formData.address.value
+        const city = formData.city.value
+        const country = formData.country.value
+        const contactName = formData.contactName.value
+        console.log(contactName)
+        const position = formData.position.value
+        const phone = formData.phone.value
+        const email = formData.email.value
+        if( !name || !address || !city || !country || !contactName || !position || !phone || !email)
+        alert("You must fill out all fields!")
+        axios.put(`/Warehouses/${this.state.selectedWarehouse.id}`, {
+                name: formData.name.value,
+                address: formData.address.value,
+                city: formData.city.value,
+                country: formData.country.value,
+                contactName: formData.contactName.value,
+                position: formData.position.value,
+                phone: formData.phone.value,
+                email: formData.email.value
+            })
+            .then(response => {
+                console.log(response.data)
+            })
+            .catch((error) => (error))
+            }
+       
+    
+    
+
 
     render() {
         if (!this.state.selectedWarehouse.id) return <div><p className="loading">Loading...</p></div>
@@ -69,37 +98,35 @@ class EditWarehouse extends Component {
                 <div className="warehouse-edit__container">
                     <form onSubmit={this.editSuccess} className="warehouse-edit__housedetails">
                         <div className="warehouse-edit__card">
-                        <h2 className="warehouse-edit__subheader">Warehouse Details</h2>
+                            <h2 className="warehouse-edit__subheader">Warehouse Details</h2>
 
-                        <label className="warehouse-edit__label" for="name">Warehouse Name</label>
-                        <input className="warehouse-edit__input" type="name" name="name" id="name" placeholder={this.state.selectedWarehouse.name}></input>
+                            <label className="warehouse-edit__label">Warehouse Name</label>
+                            <input className="warehouse-edit__input" type="name" name="name" id="name" placeholder={this.state.selectedWarehouse.name}></input>
 
-                        <label className="warehouse-edit__label">Street Address</label>
-                        <input className="warehouse-edit__input" type="text" name="address" placeholder={this.state.selectedWarehouse.address}></input>
+                            <label className="warehouse-edit__label">Street Address</label>
+                            <input className="warehouse-edit__input" type="text" name="address" placeholder={this.state.selectedWarehouse.address}></input>
 
-                        <label className="warehouse-edit__label">City</label>
-                        <input className="warehouse-edit__input" type="text" name="city" placeholder={this.state.selectedWarehouse.city}></input>
+                            <label className="warehouse-edit__label">City</label>
+                            <input className="warehouse-edit__input" type="text" name="city" placeholder={this.state.selectedWarehouse.city}></input>
 
-                        <label className="warehouse-edit__label">Country</label>
-                        <input className="warehouse-edit__input" type="text" name="country" placeholder={this.state.selectedWarehouse.country}></input>
+                            <label className="warehouse-edit__label">Country</label>
+                            <input className="warehouse-edit__input" type="text" name="country" placeholder={this.state.selectedWarehouse.country}></input>
                         </div>
-                    </form>
 
-                    <form onSubmit={this.editSuccess} className="warehouse-edit__contactdetails">
                         <div className="warehouse-edit__card">
-                        <h2 className="warehouse-edit__subheader">Contact Details</h2>
+                            <h2 className="warehouse-edit__subheader">Contact Details</h2>
 
-                        <label className="warehouse-edit__label">Contact Name</label>
-                        <input className="warehouse-edit__input" type="text" name="contact name" placeholder={this.state.selectedWarehouse.contact.name}></input>
+                            <label className="warehouse-edit__label">Contact Name</label>
+                            <input className="warehouse-edit__input" type="text" name="contactName" placeholder={this.state.selectedWarehouse.contact.name}></input>
 
-                        <label className="warehouse-edit__label">Position</label>
-                        <input className="warehouse-edit__input" type="text" name="position" placeholder={this.state.selectedWarehouse.contact.position}></input>
+                            <label className="warehouse-edit__label">Position</label>
+                            <input className="warehouse-edit__input" type="text" name="position" placeholder={this.state.selectedWarehouse.contact.position}></input>
 
-                        <label className="warehouse-edit__label">Phone Number</label>
-                        <input className="warehouse-edit__input" type="text" name="phone" placeholder={this.state.selectedWarehouse.contact.phone}></input>
+                            <label className="warehouse-edit__label">Phone Number</label>
+                            <input className="warehouse-edit__input" type="text" name="phone" placeholder={this.state.selectedWarehouse.contact.phone}></input>
 
-                        <label className="warehouse-edit__label">Email</label>
-                        <input className="warehouse-edit__input" type="text" name="email" placeholder={this.state.selectedWarehouse.contact.email}></input>
+                            <label className="warehouse-edit__label">Email</label>
+                            <input className="warehouse-edit__input" type="text" name="email" placeholder={this.state.selectedWarehouse.contact.email}></input>
                         </div>
 
                         <div className="warehouse-edit__buttons">
--- server/routes/Warehouses.js
+++ server/routes/Warehouses.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router();
 const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 
 let warehouses = fs.readFileSync('./data/Warehouses.json')
 warehouses = JSON.parse(warehouses);
@@ -12,17 +13,17 @@ let warehouseArray = warehouses.map((warehouse) => {
         address: warehouse.address,
         city: warehouse.city,
         country: warehouse.country
-        // contact: warehouse.contact
     }
     return warehouseList
 })
 
-router.get('/', (req, res) => {
-    (res.json(warehouses))
-    
+router.route('/')
+    .get ((req, res) => {
+    (res.json(warehouses)) 
 })
 
-router.get('/:warehouseId', (req, res) => {
+router.route('/:warehouseId') 
+    .get ((req, res) => {
     const warehouse = warehouses.find(warehouse => warehouse.id === req.params.warehouseId)
     if (!warehouse) {
         return res.status(404)
@@ -31,6 +32,28 @@ router.get('/:warehouseId', (req, res) => {
         })
     }
     return res.json(warehouse)
+})
+    
+    .put ((req, res) => {
+    const warehouse = warehouses.find(warehouse => warehouse.id === req.params.warehouseId)
+    const { name, address, city, country, contactName, position, phone, email } = req.body;
+    if ( name==="" || address==="" || city==="" || country==="" || contactName==="" || position==="" || phone==="" || email==="") {
+        return res.status(404)
+        .json({
+            errorMessage: `Warehouse ${req.params.warehouseId} not found`
+        })
+    }
+    warehouse.name = name
+    warehouse.address = address
+    warehouse.city = city
+    warehouse.country = country
+    warehouse.contact.name = contactName
+    warehouse.contact.position = position
+    warehouse.contact.phone = phone
+    warehouse.contact.email = email
+
+    fs.writeFileSync('data/warehouses.json', JSON.stringify(warehouses))
+    return res.send(warehouses).status(200)
 })
 
 
