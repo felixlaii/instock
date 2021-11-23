b1f14714f431439079b9c0b442129e9815292744
hamisk <hs.khera@icloud.com>
2021-11-22 16:11:17 -0800

    inventory item page loads item using params

--- client/src/pages/WarehouseDetails/WarehouseDetailsPage.jsx
+++ client/src/pages/WarehouseDetails/WarehouseDetailsPage.jsx
@@ -1,11 +1,6 @@
 import { Component } from 'react';
 import axios from "axios";
 import sortIcon from '../../assets/icons/sort-24px.svg'
-// import back from '../../assets/icons/arrow_back-24px.svg'
-// import editWhite from '../../assets/icons/edit-24px-white.svg'
-// import edit from '../../assets/icons/edit-24px.svg'
-// import deleteIcon from '../../assets/icons/delete_outline-24px.svg'
-// import chevron from '../../assets/icons/chevron_right-24px.svg'
 
 import './WarehouseDetailsPage.scss';
 import InventoryItem from '../../components/InventoryItem/InventoryItem';
@@ -21,21 +16,19 @@ class WarehouseDetailsPage extends Component {
     }
 
     componentDidMount() {
-        let warehouseId = this.props.match.params.id || "5bf7bd6c-2b16-4129-bddc-9d37ff8539e9"
+        let warehouseId = this.props.match.params.id 
 
         axios
-          .get(warehouseEndpoint + warehouseId)
-          .then(response => {
-            this.setState({
-              warehouse: response.data
+            .get(warehouseEndpoint + warehouseId)
+            .then(response => {
+                this.setState({
+                warehouse: response.data
+                })
             })
-            console.log(this.state.warehouse)
-          })
-      }
+    }
 
     render() {
         const { warehouse } = this.state
-        console.log(warehouse)
         
         if (!warehouse.length) {
             return (
@@ -44,46 +37,45 @@ class WarehouseDetailsPage extends Component {
             }
             
         if (warehouse.length) {
-        // const { category, id, itemName, quantity, status } = warehouse[1][2];
-        return (
-            <>
-                <div className="details__card">
+            return (
+                <>
+                    <div className="details__card">
 
-                    <WarehouseDetails warehouse={warehouse}/>
-                    
-                    <div className="inventory__table-row">
-                        <div className="inventory__inv-cat-subs">
-                            <p className="inventory__table-inv-sub">Inventory Item
-                                <img className="inventory__sort-icon" src={sortIcon} alt="sort icon" />
-                            </p>
-                            <p className="inventory__table-cat-sub">Category
-                                <img className="inventory__sort-icon" src={sortIcon} alt="sort icon" />
-                            </p>
+                        <WarehouseDetails warehouse={warehouse}/>
+
+                        <div className="inventory__table-row">
+                            <div className="inventory__inv-cat-subs">
+                                <p className="inventory__table-inv-sub">Inventory Item
+                                    <img className="inventory__sort-icon" src={sortIcon} alt="sort icon" />
+                                </p>
+                                <p className="inventory__table-cat-sub">Category
+                                    <img className="inventory__sort-icon" src={sortIcon} alt="sort icon" />
+                                </p>
+                            </div>
+                            <div className="inventory__stat-qty-subs">
+                                <p className="inventory__table-stat-sub">Status
+                                    <img className="inventory__sort-icon" src={sortIcon} alt="sort icon" />
+                                </p>
+                                <p className="inventory__table-sub">Quantity
+                                    <img className="inventory__sort-icon" src={sortIcon} alt="sort icon" />
+                                </p>
+                            </div>
+                            <p className="inventory__table-action-sub">Actions</p>
                         </div>
-                        <div className="inventory__stat-qty-subs">
-                            <p className="inventory__table-stat-sub">Status
-                                <img className="inventory__sort-icon" src={sortIcon} alt="sort icon" />
-                            </p>
-                            <p className="inventory__table-sub">Quantity
-                                <img className="inventory__sort-icon" src={sortIcon} alt="sort icon" />
-                            </p>
-                        </div>
-                        <p className="inventory__table-action-sub">Actions</p>
+
+                        {warehouse[1].map(inventory => 
+                            <InventoryItem key={inventory.id} category={inventory.category} 
+                                id={inventory.id} 
+                                itemName={inventory.itemName} 
+                                quantity={inventory.quantity} 
+                                status={inventory.status}
+                                warehouseName={inventory.warehouseName}
+                                showWarehouse={this.state.showWarehouse} />)}
+
                     </div>
-
-                    {warehouse[1].map(inventory => 
-                        <InventoryItem key={inventory.id} category={inventory.category} 
-                            id={inventory.id} 
-                            itemName={inventory.itemName} 
-                            quantity={inventory.quantity} 
-                            status={inventory.status}
-                            warehouseName={inventory.warehouseName}
-                            showWarehouse={this.state.showWarehouse} />)}
-
-                </div>
-            </>
-        )
-        }
+                </>
+            )
+            }
     }
 }
 
