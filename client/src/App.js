7b6ee55168d1fb388800db97673a93594bc09358
hamisk <hs.khera@icloud.com>
2021-11-22 19:18:47 -0800

    adding routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,11 @@ import WarehouseDetailsPage from './pages/WarehouseDetails/WarehouseDetailsPage'
 import PageHeader from '../src/components/PageHeader/PageHeader';
 import EditWarehouse from '../src/pages/EditWarehouse/EditWarehouse'
 import WarehouseList from './pages/WarehouseList/WarehouseList';
-import WarehouseDetails from '../src/components/WarehouseDetails/WarehouseDetails'
 import AddWarehouse from '../src/pages/AddWarehouse/AddWarehouse'
 import InventoryPage from './pages/InventoryPage/InventoryPage';
 import AddInventory from './pages/AddInventoryItem/AddInventoryItem';
 import EditInventoryItem from './pages/EditInventoryItem/EditInventoryItem.jsx'
 import InventoryItemDetails from './pages/InventoryItemDetails/InventoryItemDetails';
-import AddInventoryItem from './pages/AddInventoryItem/AddInventoryItem';
 import Footer from './components/Footer/Footer';
 import './styles/partials/_global.scss'
 
@@ -29,11 +27,12 @@ class App extends Component {
                 render={(routerProps) => { return <WarehouseDetailsPage {...routerProps} /> }} />
               <Route path="/edit-warehouse/:id"
                 render={(routerProps) => { return <EditWarehouse {...routerProps} /> }} />
-              <Route path="/add-new-warehouse" />
+              <Route path="/add-new-warehouse" component={AddWarehouse}/>
               <Route path="/inventory" exact component={InventoryPage} />
               <Route path="/inventory-item-details/:id" 
                 render={(routerProps) => { return <InventoryItemDetails {...routerProps} /> }} />
-              <Route path="/edit-inventory-item/:id" />
+              <Route path="/edit-inventory-item/:id" 
+                render={(routerProps) => { return <EditInventoryItem {...routerProps} /> }} />
               <Route path="/add-new-inventory-item" component={AddInventory} />
             </Switch>
           </div>
